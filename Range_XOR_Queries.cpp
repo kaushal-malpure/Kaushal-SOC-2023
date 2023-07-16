5dcf3326cc92d307db0ae3b7d2c793a58193213a
kaushal-malpure <71086178+kaushal-malpure@users.noreply.github.com>
2023-07-16 12:23:46 +0530

    Add files via upload

---
+++ Range_XOR_Queries.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int MAX_N = 2e5;
+
+int n, q, arr[MAX_N + 1], prefix[MAX_N + 1], a, b;
+int main() {
+	cin >> n >> q;
+	
+	for (int i = 1; i <= n; ++i) { cin >> arr[i]; }
+	prefix[1] = arr[1];
+	for (int i = 2; i <= n; i++) { prefix[i] = prefix[i - 1] ^ arr[i]; }
+
+	while (q--) {
+		cin >> a >> b;
+		cout << (prefix[a - 1] ^ prefix[b]) << endl;
+	}
+}
